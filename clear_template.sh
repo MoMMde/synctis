ff177177ef88c66b1d1f5b75d1c5e0d14fad1839
mommde <mommde@protonmail.com>
2024-06-02 14:20:21 +0200

    self destroying clear script for bash

---
+++ clear_template.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This file for bash will clean up the template to start with no annoying branding.
+# When executing this, following steps will be executed:
+# - Empty README.md
+# - Remove this 'clear_template.sh' file
+
+
+echo "> Clearing README.md"
+echo "" > README.md
+
+rm clear_template.sh
